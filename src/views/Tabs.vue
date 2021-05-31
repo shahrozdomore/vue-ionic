ae06af5a55b86b56f6fc3e3285ddac8bc7ca8516
Shaharoz Ali Khan <shahrozdomore@gmail.com>
2021-05-31 23:16:33 +0500

    initial commit

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -4,17 +4,14 @@
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
         </ion-tab-button>
-          
+
         <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
         </ion-tab-button>
-        
+
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -22,18 +19,21 @@
 </template>
 
 <script lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { IonTabBar, IonTabButton, IonTabs, IonIcon, IonPage } from "@ionic/vue";
+import { ellipse, square, triangle } from "ionicons/icons";
 
 export default {
-  name: 'Tabs',
-  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
+  name: "Tabs",
+  components: { IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
   setup() {
     return {
-      ellipse, 
-      square, 
+      ellipse,
+      square,
       triangle,
-    }
-  }
-}
+    };
+  },
+};
 </script>
+
+// git branch -M main // git remote add origin
+https://github.com/shahrozdomore/vue-ionic.git // git push -u origin main
